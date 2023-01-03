ce1819bab093d6dfadac66c40385613ae773aecd
Sakar Chauhan <saakarchauhan@gmail.com>
2023-01-03 08:26:34 +0530

    Assignment 5 to find Pair With Given Difference done.

---
+++ Week-7/Assignment-5/index.js
@@ -0,0 +1,21 @@
+console.log('Pair With Given Difference');
+
+function pairDifferenceExist(A,B){
+    let result = new Set(A);
+    if(B===0){
+        if(result.size < A.length){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+    for(let element of A){
+        if(result.has(element+B)){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+console.log('Ans :>> ', pairDifferenceExist([5, 10, 3, 2, 50, 80],78));
+console.log('Ans :>> ', pairDifferenceExist([-10,20],30));

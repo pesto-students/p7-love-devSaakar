018c5fe0550da6f5b49185400843ebfeb2d78c7f
Sakar Chauhan <saakarchauhan@gmail.com>
2023-01-12 11:18:05 +0530

    feat : implemented Queue from two stacks.

--- Week-8/Stack.js
+++ Week-8/Stack.js
@@ -2,15 +2,21 @@
 class Stack{
     constructor(arr=[]){
         this.stack = [...arr];
+        this.length = arr.length;
     }
     push(value){
+        this.length++;
         this.stack.push(value);
     }
     pop(){
-        return this.stack.pop();
+        if(this.length){
+            this.length--;
+            return this.stack.pop();
+        }
+        return "No Element to Remove"
     }
     length(){
-        return this.stack.length;
+        return this.length;
     }
     peek(){
         console.log('Top Element :>> ', this.stack.at(-1));
